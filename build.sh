c4ca42f95fc9727b1c0ae132c028982660d9a7ea
Ronald Record <github@ronrecord.com>
2023-11-13 09:38:28 -0800

    Add jumbojett/openid-connect-php and example use

--- build.sh
+++ build.sh
@@ -27,6 +27,20 @@ have_comp=$(type -p composer)
   printf "\nERROR: composer not found. Exiting.\n"
   exit 1
 }
+if [ -f okta/oauth ]; then
+  source okta/oauth
+  sudo sed -i -e "s/__CLIENT_ID__/${SEND_CLIENT_ID}/" \
+              -e "s/__CLIENT_SECRET__/${SEND_CLIENT_SECRET}/" \
+              -e "s%__OIDC_ISSUER_URL__%${SEND_ISSUER_URL}%" \
+           login-openid.php
+else
+  printf "\nWARNING: Missing okta/oauth credentials file"
+  printf "\n\tProjectSend not configured for OpenID OAuth\n"
+fi
+
+find . -type d -print0 | xargs -0 sudo chmod 775
+find . -type f -print0 | xargs -0 sudo chmod 644
+sudo chown -R www-data:www-data .
 
 npm install
 composer update
